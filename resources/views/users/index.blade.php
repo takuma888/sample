4e6961124645d58272d2690ce536a5406202bcdb
Larry <22062019@qq.com>
2018-03-28 06:40:29 +0000

    Add user statuses

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 @section('title', '所有用户')
 
-@section('content');
+@section('content')
 <div class="col-md-offset-2 col-md-8">
     <h1>所有用户</h1>
     <ul class="users">
